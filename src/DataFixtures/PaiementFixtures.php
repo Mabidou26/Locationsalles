d748e2b0099c6b8aea605424e4c10ec1836a3505
Mabidou26 <msoum92@gmail.com>
2026-02-04 14:38:43 +0100

    Remove ApiResource annotations from PaiementFixtures
    Removed ApiResource annotations and related operations from PaiementFixtures.

--- src/DataFixtures/PaiementFixtures.php
+++ src/DataFixtures/PaiementFixtures.php
@@ -6,23 +6,8 @@ use App\Entity\Paiement;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
-use ApiPlatform\Metadata\ApiResource;
-use ApiPlatform\Metadata\GetCollection;
-use ApiPlatform\Metadata\Post;
-use ApiPlatform\Metadata\Put;
-use ApiPlatform\Metadata\Get;
-use ApiPlatform\Metadata\Delete;
 
 
-#[ApiResource(
-    operations: [
-        new GetCollection(security: "is_granted('ROLE_ADMIN')"),
-        new Get(security: "is_granted('ROLE_ADMIN') or object == user"),
-        new Post(security: "is_granted('ROLE_ADMIN')"),
-        new Put(security: "is_granted('ROLE_ADMIN') or object == user"),
-        new Delete(security: "is_granted('ROLE_ADMIN')")
-    ]
-)]
 class PaiementFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
